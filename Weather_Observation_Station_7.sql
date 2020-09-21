76c95baf34e86b5552c8295a758aba9d768b88a6
Amandeep Singh <47387270+Amandeepanmol@users.noreply.github.com>
2020-09-21 21:43:30 +0530

    Create Weather_Observation_Station_7.sql

---
+++ Weather_Observation_Station_7.sql
@@ -0,0 +1,3 @@
+Weather Observation Station 7
+-------------------------------
+SELECT DISTINCT(CITY) FROM STATION WHERE CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE  '%i' OR CITY LIKE '%o' OR CITY LIKE '%u';
